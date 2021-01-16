20b43fd5fa047b65237eaaf2b05f672bdb541fad
Mantvydas Jurasius <mantvydas.jurasius@mjds.ltd>
2021-01-16 14:48:16 +0000

    Staking gas savings

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -66,8 +66,8 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
         address subBalances;
     }
 
-    Addresses public addresses;
-    IStakingV1 public stakingV1;
+    Addresses internal addresses;
+    IStakingV1 internal stakingV1;
 
     /** Roles */
     bytes32 public constant MIGRATOR_ROLE = keccak256('MIGRATOR_ROLE');
@@ -78,23 +78,23 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
     /** Public Variables */
     uint256 public shareRate;
     uint256 public sharesTotalSupply;
-    uint256 public nextPayoutCall;
+    uint256 internal nextPayoutCall;
     uint256 public stepTimestamp;
     uint256 public startContract;
-    uint256 public globalPayout;
-    uint256 public globalPayin;
-    uint256 public lastSessionId;
-    uint256 public lastSessionIdV1;
+    uint256 internal globalPayout;
+    uint256 internal globalPayin;
+    uint256 internal lastSessionId;
+    uint256 internal lastSessionIdV1;
 
     /** Mappings / Arrays */
     mapping(address => mapping(uint256 => Session)) public sessionDataOf;
     mapping(address => uint256[]) public sessionsOf;
-    Payout[] public payouts;
+    Payout[] internal payouts;
 
     /** Booleans */
-    bool public init_;
+    bool internal init_;
 
-    uint256 public basePeriod;
+    uint256 internal basePeriod;
     uint256 public totalStakedAmount;
 
     /* New variables must go below here. */
@@ -119,62 +119,49 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
         _;
     }
 
-    /** Init functions */
-    function initialize(address _manager, address _migrator)
-        public
-        initializer
-    {
-        _setupRole(MANAGER_ROLE, _manager);
-        _setupRole(MIGRATOR_ROLE, _migrator);
-        init_ = false;
+    function makePayout() internal {
+        require(now >= nextPayoutCall, 'Staking: Wrong payout time');
+
+        uint256 payout = _getPayout();
+
+        payouts.push(
+            Payout({payout: payout, sharesTotalSupply: sharesTotalSupply})
+        );
+
+        nextPayoutCall = nextPayoutCall.add(stepTimestamp);
+
+        emit MakePayout(payout, sharesTotalSupply, now);
     }
 
-    function init(
-        address _mainTokenAddress,
-        address _auctionAddress,
-        address _subBalancesAddress,
-        address _foreignSwapAddress,
-        address _stakingV1Address,
-        uint256 _stepTimestamp,
-        uint256 _lastSessionIdV1
-    ) external onlyMigrator {
-        require(!init_, 'Staking: init is active');
-        init_ = true;
-        /** Setup */
-        _setupRole(EXTERNAL_STAKER_ROLE, _foreignSwapAddress);
-        _setupRole(EXTERNAL_STAKER_ROLE, _auctionAddress);
+    function _getPayout() internal returns (uint256) {
+        uint256 amountTokenInDay =
+            IERC20Upgradeable(addresses.mainToken).balanceOf(address(this));
 
-        addresses = Addresses({
-            mainToken: _mainTokenAddress,
-            auction: _auctionAddress,
-            subBalances: _subBalancesAddress
-        });
+        globalPayin = globalPayin.add(amountTokenInDay);
 
-        stakingV1 = IStakingV1(_stakingV1Address);
-        lastSessionId = _lastSessionIdV1;
-
-        stepTimestamp = _stepTimestamp;
-
-        if (startContract == 0) {
-            startContract = now;
-            nextPayoutCall = startContract.add(_stepTimestamp);
+        if (globalPayin > globalPayout) {
+            globalPayin = globalPayin.sub(globalPayout);
+            globalPayout = 0;
+        } else {
+            globalPayin = 0;
+            globalPayout = 0;
         }
-        if (_lastSessionIdV1 != 0) {
-            lastSessionIdV1 = _lastSessionIdV1;
-        }
-        if (shareRate == 0) {
-            shareRate = 1e18;
-        }
-    }
 
-    /** End init functions */
+        uint256 currentTokenTotalSupply =
+            (IERC20Upgradeable(addresses.mainToken).totalSupply()).add(
+                globalPayin
+            );
 
-    function sessionsOf_(address account)
-        external
-        view
-        returns (uint256[] memory)
-    {
-        return sessionsOf[account];
+        IToken(addresses.mainToken).burn(address(this), amountTokenInDay);
+
+        uint256 inflation =
+            uint256(8).mul(currentTokenTotalSupply.add(totalStakedAmount)).div(
+                36500
+            );
+
+        globalPayin = globalPayin.add(inflation);
+
+        return amountTokenInDay.add(inflation);
     }
 
     function stake(uint256 amount, uint256 stakingDays) external {
@@ -219,27 +206,70 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
         );
     }
 
-    function _initPayout(address to, uint256 amount) internal {
-        IToken(addresses.mainToken).mint(to, amount);
-        globalPayout = globalPayout.add(amount);
+    function restake(uint256 sessionId, uint256 stakingDays) external {
+        require(stakingDays != 0, 'Staking: Staking days < 1');
+        require(stakingDays <= 5555, 'Staking: Staking days > 5555');
+
+        Session storage session = sessionDataOf[msg.sender][sessionId];
+
+        require(
+            session.shares != 0 && session.withdrawn == false,
+            'Staking: Stake withdrawn/invalid'
+        );
+
+        uint256 actualEnd = now;
+
+        require(session.end <= actualEnd, 'Staking: Stake not mature');
+
+        uint256 amountOut = unstakeInternal(session, sessionId, actualEnd);
+
+        stakeInternal(amountOut, stakingDays, msg.sender);
     }
 
-    function calculateStakingInterest(
-        uint256 firstPayout,
-        uint256 lastPayout,
-        uint256 shares
-    ) public view returns (uint256) {
-        uint256 stakingInterest;
-        uint256 lastIndex = MathUpgradeable.min(payouts.length, lastPayout);
+    function restakeV1(uint256 sessionId, uint256 stakingDays) external {
+        require(sessionId <= lastSessionIdV1, 'Staking: Invalid sessionId');
+        require(stakingDays != 0, 'Staking: Staking days < 1');
+        require(stakingDays <= 5555, 'Staking: Staking days > 5555');
 
-        for (uint256 i = firstPayout; i < lastIndex; i++) {
-            uint256 payout =
-                payouts[i].payout.mul(shares).div(payouts[i].sharesTotalSupply);
+        Session storage session = sessionDataOf[msg.sender][sessionId];
 
-            stakingInterest = stakingInterest.add(payout);
-        }
+        require(
+            session.shares == 0 && session.withdrawn == false,
+            'Staking: Stake withdrawn'
+        );
 
-        return stakingInterest;
+        (
+            uint256 amount,
+            uint256 start,
+            uint256 end,
+            uint256 shares,
+            uint256 firstPayout
+        ) = stakingV1.sessionDataOf(msg.sender, sessionId);
+
+        // Unstaked in v1 / doesn't exist
+        require(shares != 0, 'Staking: Stake withdrawn/invalid');
+
+        uint256 actualEnd = now;
+
+        require(end <= actualEnd, 'Staking: Stake not mature');
+
+        uint256 sessionStakingDays = (end - start) / stepTimestamp;
+        uint256 lastPayout = sessionStakingDays + firstPayout;
+
+        uint256 amountOut =
+            unstakeV1Internal(
+                sessionId,
+                amount,
+                start,
+                end,
+                actualEnd,
+                shares,
+                firstPayout,
+                lastPayout,
+                sessionStakingDays
+            );
+
+        stakeInternal(amountOut, stakingDays, msg.sender);
     }
 
     function unstake(uint256 sessionId) external {
@@ -302,203 +332,27 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
         _initPayout(msg.sender, amountOut);
     }
 
-    function getAmountOutAndPenalty(
-        uint256 amount,
-        uint256 start,
-        uint256 end,
-        uint256 stakingInterest
-    ) public view returns (uint256, uint256) {
-        uint256 stakingSeconds = end.sub(start);
-        uint256 stakingDays = stakingSeconds.div(stepTimestamp);
-        uint256 secondsStaked = now.sub(start);
-        uint256 daysStaked = secondsStaked.div(stepTimestamp);
-        uint256 amountAndInterest = amount.add(stakingInterest);
-
-        // Early
-        if (stakingDays > daysStaked) {
-            uint256 payOutAmount =
-                amountAndInterest.mul(secondsStaked).div(stakingSeconds);
-
-            uint256 earlyUnstakePenalty = amountAndInterest.sub(payOutAmount);
-
-            return (payOutAmount, earlyUnstakePenalty);
-            // In time
-        } else if (daysStaked < stakingDays.add(14)) {
-            return (amountAndInterest, 0);
-            // Late
-        } else if (daysStaked < stakingDays.add(714)) {
-            uint256 daysAfterStaking = daysStaked - stakingDays;
-
-            uint256 payOutAmount =
-                amountAndInterest.mul(uint256(714).sub(daysAfterStaking)).div(
-                    700
-                );
-
-            uint256 lateUnstakePenalty = amountAndInterest.sub(payOutAmount);
-
-            return (payOutAmount, lateUnstakePenalty);
-            // Nothing
-        } else {
-            return (0, amountAndInterest);
-        }
+    function _initPayout(address to, uint256 amount) internal {
+        IToken(addresses.mainToken).mint(to, amount);
+        globalPayout = globalPayout.add(amount);
     }
 
-    function makePayout() public {
-        require(now >= nextPayoutCall, 'Staking: Wrong payout time');
+    // function _getShareRate(
+    //     uint256 amount,
+    //     uint256 shares,
+    //     uint256 start,
+    //     uint256 end,
+    //     uint256 stakingInterest
+    // ) internal view returns (uint256) {
+    //     uint256 stakingDays = (end.sub(start)).div(stepTimestamp);
 
-        uint256 payout = _getPayout();
+    //     uint256 numerator =
+    //         (amount.add(stakingInterest)).mul(uint256(1819).add(stakingDays));
 
-        payouts.push(
-            Payout({payout: payout, sharesTotalSupply: sharesTotalSupply})
-        );
+    //     uint256 denominator = uint256(1820).mul(shares);
 
-        nextPayoutCall = nextPayoutCall.add(stepTimestamp);
-
-        emit MakePayout(payout, sharesTotalSupply, now);
-    }
-
-    function readPayout() external view returns (uint256) {
-        uint256 amountTokenInDay =
-            IERC20Upgradeable(addresses.mainToken).balanceOf(address(this));
-
-        uint256 currentTokenTotalSupply =
-            (IERC20Upgradeable(addresses.mainToken).totalSupply()).add(
-                globalPayin
-            );
-
-        uint256 inflation =
-            uint256(8).mul(currentTokenTotalSupply.add(totalStakedAmount)).div(
-                36500
-            );
-
-        return amountTokenInDay.add(inflation);
-    }
-
-    function _getPayout() internal returns (uint256) {
-        uint256 amountTokenInDay =
-            IERC20Upgradeable(addresses.mainToken).balanceOf(address(this));
-
-        globalPayin = globalPayin.add(amountTokenInDay);
-
-        if (globalPayin > globalPayout) {
-            globalPayin = globalPayin.sub(globalPayout);
-            globalPayout = 0;
-        } else {
-            globalPayin = 0;
-            globalPayout = 0;
-        }
-
-        uint256 currentTokenTotalSupply =
-            (IERC20Upgradeable(addresses.mainToken).totalSupply()).add(
-                globalPayin
-            );
-
-        IToken(addresses.mainToken).burn(address(this), amountTokenInDay);
-
-        uint256 inflation =
-            uint256(8).mul(currentTokenTotalSupply.add(totalStakedAmount)).div(
-                36500
-            );
-
-        globalPayin = globalPayin.add(inflation);
-
-        return amountTokenInDay.add(inflation);
-    }
-
-    function _getStakersSharesAmount(
-        uint256 amount,
-        uint256 start,
-        uint256 end
-    ) internal view returns (uint256) {
-        uint256 stakingDays = (end.sub(start)).div(stepTimestamp);
-        uint256 numerator = amount.mul(uint256(1819).add(stakingDays));
-        uint256 denominator = uint256(1820).mul(shareRate);
-
-        return (numerator).mul(1e18).div(denominator);
-    }
-
-    function _getShareRate(
-        uint256 amount,
-        uint256 shares,
-        uint256 start,
-        uint256 end,
-        uint256 stakingInterest
-    ) internal view returns (uint256) {
-        uint256 stakingDays = (end.sub(start)).div(stepTimestamp);
-
-        uint256 numerator =
-            (amount.add(stakingInterest)).mul(uint256(1819).add(stakingDays));
-
-        uint256 denominator = uint256(1820).mul(shares);
-
-        return (numerator).mul(1e18).div(denominator);
-    }
-
-    function restake(uint256 sessionId, uint256 stakingDays) external {
-        require(stakingDays != 0, 'Staking: Staking days < 1');
-        require(stakingDays <= 5555, 'Staking: Staking days > 5555');
-
-        Session storage session = sessionDataOf[msg.sender][sessionId];
-
-        require(
-            session.shares != 0 && session.withdrawn == false,
-            'Staking: Stake withdrawn/invalid'
-        );
-
-        uint256 actualEnd = now;
-
-        require(session.end <= actualEnd, 'Staking: Stake not mature');
-
-        uint256 amountOut = unstakeInternal(session, sessionId, actualEnd);
-
-        stakeInternal(amountOut, stakingDays, msg.sender);
-    }
-
-    function restakeV1(uint256 sessionId, uint256 stakingDays) external {
-        require(sessionId <= lastSessionIdV1, 'Staking: Invalid sessionId');
-        require(stakingDays != 0, 'Staking: Staking days < 1');
-        require(stakingDays <= 5555, 'Staking: Staking days > 5555');
-
-        Session storage session = sessionDataOf[msg.sender][sessionId];
-
-        require(
-            session.shares == 0 && session.withdrawn == false,
-            'Staking: Stake withdrawn'
-        );
-
-        (
-            uint256 amount,
-            uint256 start,
-            uint256 end,
-            uint256 shares,
-            uint256 firstPayout
-        ) = stakingV1.sessionDataOf(msg.sender, sessionId);
-
-        // Unstaked in v1 / doesn't exist
-        require(shares != 0, 'Staking: Stake withdrawn');
-
-        uint256 actualEnd = now;
-
-        require(end <= actualEnd, 'Staking: Stake not mature');
-
-        uint256 sessionStakingDays = (end - start) / stepTimestamp;
-        uint256 lastPayout = sessionStakingDays + firstPayout;
-
-        uint256 amountOut =
-            unstakeV1Internal(
-                sessionId,
-                amount,
-                start,
-                end,
-                actualEnd,
-                shares,
-                firstPayout,
-                lastPayout,
-                sessionStakingDays
-            );
-
-        stakeInternal(amountOut, stakingDays, msg.sender);
-    }
+    //     return (numerator).mul(1e18).div(denominator);
+    // }
 
     function unstakeInternal(
         Session storage session,
@@ -625,14 +479,63 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
         return amountOut;
     }
 
-    /** Roles management - only for multi sig address */
-    function setupRole(bytes32 role, address account) external onlyManager {
-        _setupRole(role, account);
+    function getAmountOutAndPenalty(
+        uint256 amount,
+        uint256 start,
+        uint256 end,
+        uint256 stakingInterest
+    ) public view returns (uint256, uint256) {
+        uint256 stakingSeconds = end.sub(start);
+        uint256 stakingDays = stakingSeconds.div(stepTimestamp);
+        uint256 secondsStaked = now.sub(start);
+        uint256 daysStaked = secondsStaked.div(stepTimestamp);
+        uint256 amountAndInterest = amount.add(stakingInterest);
+
+        // Early
+        if (stakingDays > daysStaked) {
+            uint256 payOutAmount =
+                amountAndInterest.mul(secondsStaked).div(stakingSeconds);
+
+            uint256 earlyUnstakePenalty = amountAndInterest.sub(payOutAmount);
+
+            return (payOutAmount, earlyUnstakePenalty);
+            // In time
+        } else if (daysStaked < stakingDays.add(14)) {
+            return (amountAndInterest, 0);
+            // Late
+        } else if (daysStaked < stakingDays.add(714)) {
+            uint256 daysAfterStaking = daysStaked - stakingDays;
+
+            uint256 payOutAmount =
+                amountAndInterest.mul(uint256(714).sub(daysAfterStaking)).div(
+                    700
+                );
+
+            uint256 lateUnstakePenalty = amountAndInterest.sub(payOutAmount);
+
+            return (payOutAmount, lateUnstakePenalty);
+            // Nothing
+        } else {
+            return (0, amountAndInterest);
+        }
     }
 
-    /** Temporary */
-    function setShareRate(uint256 _shareRate) external onlyManager {
-        shareRate = _shareRate;
+    function calculateStakingInterest(
+        uint256 firstPayout,
+        uint256 lastPayout,
+        uint256 shares
+    ) public view returns (uint256) {
+        uint256 stakingInterest;
+        uint256 lastIndex = MathUpgradeable.min(payouts.length, lastPayout);
+
+        for (uint256 i = firstPayout; i < lastIndex; i++) {
+            uint256 payout =
+                payouts[i].payout.mul(shares).div(payouts[i].sharesTotalSupply);
+
+            stakingInterest = stakingInterest.add(payout);
+        }
+
+        return stakingInterest;
     }
 
     function stakeInternalCommon(
@@ -673,4 +576,83 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
 
         emit Stake(staker, sessionId, amount, start, end, shares);
     }
+
+    function _getStakersSharesAmount(
+        uint256 amount,
+        uint256 start,
+        uint256 end
+    ) internal view returns (uint256) {
+        uint256 stakingDays = (end.sub(start)).div(stepTimestamp);
+        uint256 numerator = amount.mul(uint256(1819).add(stakingDays));
+        uint256 denominator = uint256(1820).mul(shareRate);
+
+        return (numerator).mul(1e18).div(denominator);
+    }
+
+    /** Roles management - only for multi sig address */
+    function setupRole(bytes32 role, address account) external onlyManager {
+        _setupRole(role, account);
+    }
+
+    /** Temporary */
+    function setShareRate(uint256 _shareRate) external onlyManager {
+        shareRate = _shareRate;
+    }
+
+    function sessionsOf_(address account)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        return sessionsOf[account];
+    }
+
+    /** Init functions */
+    function initialize(address _manager, address _migrator)
+        external
+        initializer
+    {
+        _setupRole(MANAGER_ROLE, _manager);
+        _setupRole(MIGRATOR_ROLE, _migrator);
+        init_ = false;
+    }
+
+    function init(
+        address _mainTokenAddress,
+        address _auctionAddress,
+        address _subBalancesAddress,
+        address _foreignSwapAddress,
+        address _stakingV1Address,
+        uint256 _stepTimestamp,
+        uint256 _lastSessionIdV1
+    ) external onlyMigrator {
+        require(!init_, 'Staking: init is active');
+        init_ = true;
+        /** Setup */
+        _setupRole(EXTERNAL_STAKER_ROLE, _foreignSwapAddress);
+        _setupRole(EXTERNAL_STAKER_ROLE, _auctionAddress);
+
+        addresses = Addresses({
+            mainToken: _mainTokenAddress,
+            auction: _auctionAddress,
+            subBalances: _subBalancesAddress
+        });
+
+        stakingV1 = IStakingV1(_stakingV1Address);
+        lastSessionId = _lastSessionIdV1;
+
+        stepTimestamp = _stepTimestamp;
+
+        if (startContract == 0) {
+            startContract = now;
+            nextPayoutCall = startContract.add(_stepTimestamp);
+        }
+        if (_lastSessionIdV1 != 0) {
+            lastSessionIdV1 = _lastSessionIdV1;
+        }
+        if (shareRate == 0) {
+            shareRate = 1e18;
+        }
+    }
+    /** End init functions */
 }
