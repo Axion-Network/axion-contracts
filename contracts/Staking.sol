dfa5749d900a154fa9a7c7f30574f76c7a7254c4
petec86 <76200781+petec86@users.noreply.github.com>
2021-01-19 19:55:54 +0200

    restake topup feature
    when doing a restake the user can select to topup the amount

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -434,7 +434,7 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
         return (numerator).mul(1e18).div(denominator);
     }
 
-    function restake(uint256 sessionId, uint256 stakingDays) external {
+    function restake(uint256 sessionId, uint256 stakingDays, uint256 topup) external {
         require(stakingDays != 0, 'Staking: Staking days < 1');
         require(stakingDays <= 5555, 'Staking: Staking days > 5555');
 
@@ -451,10 +451,15 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
 
         uint256 amountOut = unstakeInternal(session, sessionId, actualEnd);
 
+        if(topup>0){
+        IToken(addresses.mainToken).burn(msg.sender, topup);
+        amountOut = amountOut.add(topup);
+        }
+
         stakeInternal(amountOut, stakingDays, msg.sender);
     }
 
-    function restakeV1(uint256 sessionId, uint256 stakingDays) external {
+    function restakeV1(uint256 sessionId, uint256 stakingDays, uint256 topup) external {
         require(sessionId <= lastSessionIdV1, 'Staking: Invalid sessionId');
         require(stakingDays != 0, 'Staking: Staking days < 1');
         require(stakingDays <= 5555, 'Staking: Staking days > 5555');
@@ -497,6 +502,11 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
                 sessionStakingDays
             );
 
+        if(topup>0){
+        IToken(addresses.mainToken).burn(msg.sender, topup);
+        amountOut = amountOut.add(topup);
+        }
+        
         stakeInternal(amountOut, stakingDays, msg.sender);
     }
 
