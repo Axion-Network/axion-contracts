e0ab2379def0b56ba1aaa587f10e94cd6bb26d7c
petec86 <76200781+petec86@users.noreply.github.com>
2021-01-23 21:48:10 +0200

    Update Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -365,7 +365,7 @@ contract Staking is IStaking, Initializable, AccessControlUpgradeable {
 
         uint256 index = payoutPerShare.length != 0 ? payoutPerShare.length - 1 : 0;
 
-        if ( payoutPerShare.lengt!=0 ) {
+        if ( payoutPerShare.length!=0 ) {
             uint256 todaysSharePayout = payoutPerShare[index].add(payout.mul(1e12).div(sharesTotalSupply));
         } else {
             uint256 todaysSharePayout = payout.mul(1e12).div(sharesTotalSupply);
